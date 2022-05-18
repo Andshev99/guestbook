79ad3770216b6519da8bf0d59d11b0ed841b3c45
shevtsov_ajax <shevtsov@ajax.systems>
2022-05-18 17:49:01 +0300

    Чтение сегмента записей из файла

--- guestbook.go
+++ guestbook.go
@@ -1,11 +1,29 @@
-/*
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
 
 func check(err error) {
 	if err != nil {
@@ -14,6 +32,8 @@ func check(err error) {
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
+	fmt.Printf("%#v\n", signatures)
 	html, err := template.ParseFiles("view.html")
 	check(err)
 	err = html.Execute(writer, nil)
@@ -24,26 +44,4 @@ func main() {
 	http.HandleFunc("/guestbook", viewHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
-}
-*/
-package main
-
-import (
-	"log"
-	"os"
-	"text/template"
-)
-
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func main() {
-	text := "Here's my template!\n"
-	tmpl, err := template.New("test").Parse(text)
-	check(err)
-	err = tmpl.Execute(os.Stdout, nil)
-	check(err)
 }
